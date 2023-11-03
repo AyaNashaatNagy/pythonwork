bea7351db4586501d9a282b6ab6d970c1f1c5e03
AyaNashaatNagy <149835059+AyaNashaatNagy@users.noreply.github.com>
2023-11-03 22:27:51 +0200

    Add files via upload

---
+++ Aya1.py
@@ -0,0 +1,33 @@
+# st="nomiAya"
+
+# for t in range(12):
+#     if t<=10:
+#         print("print dasta until 10")
+# i= [1,2,3,4,5]
+# print(type(i))
+# i=("a,b,c,d",1,2)
+# print(type(i))
+# i={1,2,3,4}
+# print(type(i))
+    # i={'type':'4'}
+# print(type(i))
+# import threading as th
+# import time 
+# def aya(sec):
+#     print()
+# Aya = 42
+# print(f"your value is {Aya}")
+# formatted_string = f"The value of the variable is {variable}"
+
+# aya1="rai"
+# aya2=2
+# aya3=4
+# print(f"your first value is {aya1}_ your second value is {aya2} _your third value is {aya3}")
+
+# a="aya"
+# b="rai"
+# print(len(a)+len(b))
+
+
+
+
